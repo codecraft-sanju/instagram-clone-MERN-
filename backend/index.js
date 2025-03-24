0d4f6da7abd39169650c097f99233e8adc5f0cd0
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-24 21:28:30 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,23 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 import cookieparser from 'cookie-parser';
-import cors from "cors";
-import { connectDb } from "./database/db.js";
+import cors from 'cors';
+import { connectDb } from './database/db.js';
+import userRoute from './routes/userRoutes.js';
 
 const app = express();
 const PORT = 5000;
 dotenv.config();
-
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(cookieparser());
 
-// server 
+app.use("/api", userRoute);
+
+// server
 app.listen(PORT, () => {
-    console.log(`Server is running on port no. ${PORT}`);
-    connectDb();
-})
+  console.log(`Server is running on port no. ${PORT}`);
+  connectDb();
+});
